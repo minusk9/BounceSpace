4e09bdb0a705205953345784b34b7c9a987cec7a
Minus K <57159844+minusk9@users.noreply.github.com>
2021-04-28 01:34:31 +0200

    Follow PEP8

--- lib/p8.py
+++ lib/p8.py
@@ -1,36 +1,59 @@
 #!/usr/bin/python3
 
+import getopt
+import os
+import sys
+
+import bits
+import play8
+
 # Play8
 
 # P8
 # Play8 9
 # Written by Athos Toniolo
 
+
 # p8
 def p8_0(args, opts, optv):
-	if "-h" in optv:
-		sys.stderr.write("P8 (Play 8)\n\nUsage:\n\t[PATH]p8.py [-h] [--config=STRING] [--play=GAME]\n\nOptions:\n\t--config\tconfig STRING\n\t--play\tplay GAME (default bits)\n\t-h\tthis help\n")
-		sys.exit()
-	if opt_play(opts) == 'bits':
-		g = bits.BITS(os.path.join(os.path.dirname(sys.argv[0]), ".."), play8.config_parse(opt_config(opts)))
-		g.play()
-	return
+    if "-h" in optv:
+        sys.stderr.write("""
+P8 (Play 8)
+
+Usage:
+    [PATH]p8.py [-h] [--config=STRING] [--play=GAME]
+
+Options:
+    --config    config STRING
+    --play      play GAME (default bits)
+    -h          this help
+""")
+        sys.exit()
+    if opt_play(opts) == 'bits':
+        g = bits.BITS(
+            os.path.join(os.path.dirname(sys.argv[0]), ".."),
+            play8.config_parse(opt_config(opts))
+        )
+        g.play()
+    return
+
 
 # config
 def opt_config(opts):
-	for x, x2 in opts:
-		if x == "--config":
-			return x2
-	return str()
+    for x, x2 in opts:
+        if x == "--config":
+            return x2
+    return str()
+
 
 # play
 def opt_play(opts):
-	for x, x2 in opts:
-		if x == "--play":
-			return x2
-	return 'bits'
+    for x, x2 in opts:
+        if x == "--play":
+            return x2
+    return 'bits'
+
 
 # play8
-import bits, getopt, os.path, play8, sys
 opts, args = getopt.getopt(sys.argv[1:], "h", ["config=", "play="])
 p8_0(args, opts, [x for x, x2 in opts])
--- lib/play8.py
+++ lib/play8.py
@@ -4,87 +4,93 @@
 # Play8 9
 # Written by Athos Toniolo
 
-# use os path pygame pygame image
 import os.path
 import pygame
 import pygame.image
 
-# bytes shape
-def bytes_shape(line, size):
-	l = list()
-	for x in range(0, len(line), size):
-		i = x + size
-		l.append(line[x:i])
-	return l
 
-# config parse
+def bytes_shape(line, size):
+    lst = list()
+    for x in range(0, len(line), size):
+        i = x + size
+        lst.append(line[x:i])
+    return lst
+
+
 def config_parse(config):
-	d = dict()
-	for x in config.split(';'):
-		if x:
-			l = x.split(':', 1)
-			if 1 < len(l):
-				d[l[0]] = l[1]
-			else:
-				d[x] = str()
-	return d
+    d = dict()
+    for x in config.split(';'):
+        if x:
+            lst = x.split(':', 1)
+            if 1 < len(lst):
+                d[lst[0]] = lst[1]
+            else:
+                d[x] = str()
+    return d
+
 
 # screen64 screen
 def screen64_screen(fore):
-	if fore:
-		return 108, 94, 181
-	return 53, 40, 121
+    if fore:
+        return 108, 94, 181
+    return 53, 40, 121
+
 
 # screen64 font
 def screen64_font(path, bank):
-	s = "bank_{0}.p8.png"
-	i = pygame.image.load(os.path.join(path, "res", s.format(bank)))
-	return i.convert()
+    s = "bank_{0}.p8.png"
+    i = pygame.image.load(os.path.join(path, "res", s.format(bank)))
+    return i.convert()
+
 
 # screen64 make
 def screen64_make(font, rows):
-	i = len(rows[0])
-	t = 16 * i, 16 * len(rows)
-	s = pygame.Surface(t)
-	i = 0
-	for x in rows:
-		t = 0, i
-		screen64_printrow(font, x, s, t)
-		i += 1
-	return s
+    i = len(rows[0])
+    t = 16 * i, 16 * len(rows)
+    s = pygame.Surface(t)
+    i = 0
+    for x in rows:
+        t = 0, i
+        screen64_printrow(font, x, s, t)
+        i += 1
+    return s
+
 
 # screen64 view
 def screen64_view(size, back=None):
-	i, i2 = size
-	t = 0, 0
-	t2 = 16 * i, 16 * i2
-	if back:
-		return back.subsurface(pygame.Rect(t, t2))
-	pygame.display.set_mode(t2)
-	return pygame.display.get_surface()
+    i, i2 = size
+    t = 0, 0
+    t2 = 16 * i, 16 * i2
+    if back:
+        return back.subsurface(pygame.Rect(t, t2))
+    pygame.display.set_mode(t2)
+    return pygame.display.get_surface()
+
 
 # screen64 printall
 def screen64_printall(font, rows, dest, spos):
-	i, i2 = spos
-	for x in rows:
-		t = i, i2
-		screen64_printrow(font, x, dest, t)
-		i2 += 1
-	return i, i2
+    i, i2 = spos
+    for x in rows:
+        t = i, i2
+        screen64_printrow(font, x, dest, t)
+        i2 += 1
+    return i, i2
+
 
 # screen64 printrow
 def screen64_printrow(font, line, dest, spos):
-	i, i2 = spos
-	for x in line:
-		t = 16 * i, 16 * i2
-		r = pygame.Rect(16 * x, 0, 16, 16)
-		dest.blit(font, t, r)
-		i += 1
-	return i, i2
+    i, i2 = spos
+    for x in line:
+        t = 16 * i, 16 * i2
+        r = pygame.Rect(16 * x, 0, 16, 16)
+        dest.blit(font, t, r)
+        i += 1
+    return i, i2
+
 
 # screen64 progress
 def screen64_progress(font, rvrs, progress, line, dest, spos):
-	i = int(progress * len(line))
-	r = screen64_printrow(rvrs, line[:i], dest, spos)
-	screen64_printrow(font, line[i:], dest, r)
-	return
+    i = int(progress * len(line))
+    r = screen64_printrow(rvrs, line[:i], dest, spos)
+    screen64_printrow(font, line[i:], dest, r)
+    return
